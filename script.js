7922758f8d0e61cb18079599efa057de27d10f44
osamma1 <155220658+osamma1@users.noreply.github.com>
2024-07-06 09:04:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav a');
+    for (const link of links) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
         });
-    });
+    }
 });
